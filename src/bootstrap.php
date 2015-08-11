48a940028b66830446d6c0f6f09c4e425197df7c
Houston VPS <tom@muska.co.uk>
2015-08-11 16:54:19 +0400

    Loading relevant application config based on "env" environment variable

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -41,7 +41,7 @@ $app->register(new ServiceControllerServiceProvider());
 
 // Register Monolog service provider for local logging
 $app->register(new MonologServiceProvider(), array(
-    'monolog.logfile' => DOCUMENT_ROOT.LOG_PATH,
+    'monolog.logfile' => __DIR__.LOG_PATH,
 	'monolog.name' => APP_NAME,
 	'monolog.level' => LOG_LEVEL
 ));
@@ -64,6 +64,6 @@ $app->error(function(\Exception $e, $code) use ($app) {
 });
 
 // Load routes
-foreach(glob(DOCUMENT_ROOT . "/src/Kineo/Route/*.php") as $filename) {
+foreach(glob(__DIR__ . "/Kineo/Route/*.php") as $filename) {
     require_once $filename;
 }
--- src/config-loader.php
+++ src/config-loader.php
@@ -1,6 +1,21 @@
 <?php
-// Import application config
-$ini_array = parse_ini_file(__DIR__ . '/config.ini');
-foreach($ini_array as $key => $val) {
+// Import application config	
+switch(getenv('env')) {
+	case 'test':
+		$configFile = 'config-test.ini';
+		break;
+	
+	case 'development':
+	case 'production':
+		$configFile = 'config.ini';
+		break;
+	
+	default:
+		$configFile = 'config.ini';
+}
+
+$iniArray = parse_ini_file(__DIR__ . DIRECTORY_SEPARATOR . $configFile);
+
+foreach($iniArray as $key => $val) {
 	define($key, $val);
 }
